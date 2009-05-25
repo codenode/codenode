ca9166da60f5151139da26900a53caeca67083cc
Alex Clemesha <clemesha@gmail.com>
2009-05-25 01:38:13 -0700

    Correctly handle initial settings values not existing - put logic in template for 'open nb in new window'

--- knoboo/apps/usersettings/models.py
+++ knoboo/apps/usersettings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserSettings(models.Model):
-    notebook_opens_in_new_window = models.BooleanField(default=True)
+    notebook_opens_in_new_window = models.BooleanField(default=False)
     user = models.ForeignKey(User, unique=True)
 
 
