865d4bd1404316ca28a955a167dc306a1d88acc1
Alex Clemesha <clemesha@gmail.com>
2009-07-30 02:24:15 -0700

    Working revisioning of Notebooks and Cells, with revert functionality.

--- codenode/frontend/notebook/urls.py
+++ codenode/frontend/notebook/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
 
-from codenode.frontend.notebook.views import notebook, share, revisions
+from codenode.frontend.notebook.views import notebook, share, revisions, revert
 
 urlpatterns = patterns('',
     url(r'^(?P<nbid>\w{32})/$', notebook, name='notebook'),
     url(r'^share/(?P<nbid>\w{32})$', share, name='share'),
     url(r'^revisions/(?P<nbid>\w{32})$', revisions, name='revisions'),
+    url(r'^revert/(?P<id>\d+)$', revert, name='revert'),
 )
