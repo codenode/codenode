cf615667549b76f3c172e7b0109763b572361e92
James Casbon <james.casbon@popgentech.com>
2009-11-24 22:03:41 +0000

    search index create destroy

--- codenode/frontend/__init__.py
+++ codenode/frontend/__init__.py
@@ -2,13 +2,15 @@
 # Importing is done inside the function to prevent anything funny happening when importing this
 # file.
 
-def setUpPackage(self):
+def setUpPackage(self):    
+    # create a search index
     from codenode.frontend.search import search
     import tempfile
     search.SEARCH_INDEX = tempfile.mktemp() # do not create the file, otherwise index is not created
     search.create_index()
     
 def tearDownPackage():
+    # delete search index
     from codenode.frontend.search import search
     import shutil
     shutil.rmtree(search.SEARCH_INDEX)
