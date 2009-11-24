16f5e485f0278f50c01f1a084a69eaeed847b899
James Casbon <james.casbon@popgentech.com>
2009-11-24 21:53:15 +0000

    package level test setup/teardown that creates/cleans a search index

---
+++ codenode/frontend/__init__.py
@@ -0,0 +1,14 @@
+# These are package level test setups that run before and after all frontend testing.
+# Importing is done inside the function to prevent anything funny happening when importing this
+# file.
+
+def setUpPackage(self):
+    from codenode.frontend.search import search
+    import tempfile
+    search.SEARCH_INDEX = tempfile.mktemp() # do not create the file, otherwise index is not created
+    search.create_index()
+    
+def tearDownPackage():
+    from codenode.frontend.search import search
+    import shutil
+    shutil.rmtree(search.SEARCH_INDEX)
